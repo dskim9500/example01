760ff8305a88f39a20cf19e5ceaae519b8e6e296
dskim <dskim@hist.co.kr>
2017-04-19 17:53:22 +0900

    입출력

---
+++ src/org/opentutorials/javatutorials/io/InputForeachDemo.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.io;
+
+class InputForeachDemo{
+    public static void main(String[] args){
+        for(String e : args){
+            System.out.println(e);
+        }
+    }
+}
+
